dcbbb948b892bbb7ddb7560cb99153edc6baf9d8
KnifeF <knifef@protonmail.com>
2023-08-13 15:40:09 +0300

    add treasure island ascii game

---
+++ README.md
@@ -0,0 +1,3 @@
+Some small scripts and console-based apps written in Python.
+1. Treasure Island (using ascii art found on the internet).
+2. 
---
+++ treasure-island/treasure_island_ascii.py
@@ -0,0 +1,254 @@
+# A terrible virus variant strikes back (some version of "Treasure Island")
+"""
+modified some text within ascii art from:
+https://ascii.co.uk/art/starwars ; https://ascii.co.uk/art/starwars1 https://ascii.co.uk/art/yoda ;
+https://ascii.co.uk/art/video ; https://ascii.co.uk/art/pyjamas ; https://ascii.co.uk/art/dancing ;
+https://ascii.co.uk/art/injury ; https://ascii.co.uk/art/bat ; https://ascii.co.uk/art/apple ;
+https://ascii.co.uk/art/running ; https://ascii.co.uk/text
+"""
+
+p1 = "Darth Vader"
+p2 = "Yoda"
+zoom = "Zoom"
+tiktok = "TikTok"
+bat = "Bat Soup"
+garlic = "Garlic"
+apple = "Apple"
+snow_white = "Snow White and the Seven Dwarfs"
+star_wars_ascii_art = '''
+    .                          .                              .
+ .             o88888888888888  d88b  .  8888888b.  .                   .
+       .  .    Y88<""""888"""" j8PY8i    888   )88               .
+                Y8b.   888    ,8P  Y8,   88888888'         .
+_________________>88b  888    88888888   888  Y8b___________________________
+:::::::::88888888888P  888   d8P    Y8b  888   Y888888::::::::::::::::::::::
+:::::::::(                                           )::::::::::::::::::::::
+:::::::::Y8b  d88b  d8P  d88b   . 8888888b.  o88888888::::::::::::::::::::::
+""""""""""88ij8888ij88' j8PY8i    888   )88  Y88<"""""""""""""""""""""""""""
+          "8888PY8888' ,8P  Y8,   88888888'   Y8b.   .          .
+    .      Y88P  Y88P  88888888   888  Y8b_____>88b           .           LS
+  .      .  Y8'  "8P  d8P    Y8b  888   Y888888888P                       .                                .            .
+'''
+darth_vader_ascii_art = '''
+   _________________________________
+  |:::::::::::::;;::::::::::::::::::|
+  |:::::::::::'~||~~~``:::::::::::::|
+  |::::::::'   .':     o`:::::::::::|
+  |:::::::' oo | |o  o    ::::::::::|
+  |::::::: 8  .'.'    8 o  :::::::::|
+  |::::::: 8  | |     8    :::::::::|
+  |::::::: _._| |_,...8    :::::::::|
+  |::::::'~--.   .--. `.   `::::::::|
+  |:::::'     =8     ~  \ o ::::::::|
+  |::::'       8._ 88.   \ o::::::::|
+  |:::'   __. ,.ooo~~.    \ o`::::::|
+  |:::   . -. 88`78o/:     \  `:::::|
+  |::'     /. o o \ ::      \88`::::|   "He will join us or die."
+  |:;     o|| 8 8 |d.        `8 `:::|
+  |:.       - ^ ^ -'           `-`::|
+  |::.                          .:::|
+  |:::::.....           ::'     ``::|
+  |::::::::-'`-        88          `|
+  |:::::-'.          -       ::     |
+  |:-~. . .                   :     |
+  | .. .   ..:   o:8      88o       |
+  |. .     :::   8:P     d888. . .  |
+  |.   .   :88   88      888'  . .  |
+  |   o8  d88P . 88   ' d88P   ..   |
+  |  88P  888   d8P   ' 888         |
+  |   8  d88P.'d:8  .- dP~ o8       |   Darth Vader (1)
+  |      888   888    d~ o888    LS |
+  |_________________________________|
+'''
+yoda_ascii_art = '''
+                   ____                  
+                _.' :  `._               
+            .-.'`.  ;   .'`.-.           
+   __      / : ___\ ;  /___ ; \      __  
+ ,'_ ""--.:__;".-.";: :".-.":__;.--"" _`,
+ :' `.t""--.. '<@.`;_  ',@>` ..--""j.' `;
+      `:-.._J '-.-'L__ `-- ' L_..-;'     
+        "-.__ ;  .-"  "-.  : __.-"       
+            L ' /.------.\ ' J           
+             "-.   "--"   .-"            
+            __.l"-:_JL_;-";.__           
+         .-j/'.;  ;""""  / .'\"-.        
+       .' /:`. "-.:     .-" .';  `.      
+    .-"  / ;  "-. "-..-" .-"  :    "-.   
+ .+"-.  : :      "-.__.-"      ;-._   \  
+ ; \  `.; ;                    : : "+. ; 
+ :  ;   ; ;                    : ;  : \: 
+ ;  :   ; :                    ;:   ;  : 
+: \  ;  :  ;                  : ;  /  :: 
+;  ; :   ; :                  ;   :   ;: 
+:  :  ;  :  ;                : :  ;  : ; 
+;\    :   ; :                ; ;     ; ; 
+: `."-;   :  ;              :  ;    /  ; 
+ ;    -:   ; :              ;  : .-"   : 
+ :\     \  :  ;            : \.-"      : 
+  ;`.    \  ; :            ;.'_..--  / ; 
+  :  "-.  "-:  ;          :/."      .'  :
+   \         \ :          ;/  __        :
+    \       .-`.\        /t-""  ":-+.   :
+     `.  .-"    `l    __/ /`. :  ; ; \  ;
+       \   .-" .-"-.-"  .' .'j \  /   ;/ 
+        \ / .-"   /.     .'.' ;_:'    ;  
+         :-""-.`./-.'     /    `.___.'   
+               \ `t  ._  /  bug          
+                "-.t-._:'                  -Yoda
+'''
+zoom_ascii_art = '''
+       _     _            
+      (_)   | |           
+__   ___  __| | ___  ___  
+\ \ / / |/ _` |/ _ \/ _ \ 
+ \ V /| | (_| |  __/ (_) |
+  \_/ |_|\__,_|\___|\___/ 
+
+
+     _   _
+     /)`:'(\
+    //| : |\\
+      |-'-|
+      | | |
+      |_|_|
+'''
+tiktok_ascii_art = '''
+     _                  _             
+    | |                (_)            
+  __| | __ _ _ __   ___ _ _ __   __ _ 
+ / _` |/ _` | '_ \ / __| | '_ \ / _` |
+| (_| | (_| | | | | (__| | | | | (_| |
+ \__,_|\__,_|_| |_|\___|_|_| |_|\__, |
+                                 __/ |
+                                |___/ 
+                      .---
+                     / # o
+                     \,__>
+                  .o-'-'--._
+                 / |\_      '.
+                |  |  \   -,  \
+                \  /   \__| ) |
+                 '|_____[)) |,/
+                    |===H=|\ >>
+                    \  __,| \_\
+                     \/   \  \_\
+                     |\    |  \/
+                     | \   \   \\
+                     |  \   |   \\
+                     |__|\ ,-ooD \\
+                     |--\_(\.-'   \o
+             snd     '-.__) 
+'''
+bat_ascii_art = '''
+          ,       ,
+        ./(       )\.
+        )  \/\_/\/  (
+        `)  (^Y^)  (`
+         `),-(~)-,(`  ldb
+             '"'
+'''
+apple_ascii_art = '''
+        __
+        \/.--,
+        //_.'
+   .-""-/""-.
+  /       __ \
+ /        \\\ \
+ |         || |
+ \            /
+ \  \         /
+  \  '-      /
+   '-.__.__.'     sjw
+'''
+garlic_ascii_art = '''
+everyone run away from garlic's smell
+                        ,////,
+                        /// 6|
+                        //  _|
+                       _/_,-'
+                  _.-/'/   \   ,/;,
+               ,-' /'  \_   \ / _/
+               `\ /     _/\  ` /
+                 |     /,  `\_/
+                 |     \'
+ pb  /\_        /`      /\
+   /' /_``--.__/\  `,. /  \
+  |_/`  `-._     `\/  `\   `.
+            `-.__/'     `\   |
+                          `\  \
+                            `\ \
+                              \_\__
+                               \___)
+'''
+win_ascii_art = '''
+ /$$     /$$                        /$$      /$$ /$$          
+|  $$   /$$/                       | $$  /$ | $$|__/          
+ \  $$ /$$//$$$$$$  /$$   /$$      | $$ /$$$| $$ /$$ /$$$$$$$ 
+  \  $$$$//$$__  $$| $$  | $$      | $$/$$ $$ $$| $$| $$__  $$
+   \  $$/| $$  \ $$| $$  | $$      | $$$$_  $$$$| $$| $$  \ $$
+    | $$ | $$  | $$| $$  | $$      | $$$/ \  $$$| $$| $$  | $$
+    | $$ |  $$$$$$/|  $$$$$$/      | $$/   \  $$| $$| $$  | $$
+    |__/  \______/  \______/       |__/     \__/|__/|__/  |__/
+'''
+gameover_ascii_art = '''
+ dP""b8    db    8b    d8 888888      dP"Yb  Yb    dP 888888 88""Yb 
+dP   `"   dPYb   88b  d88 88__       dP   Yb  Yb  dP  88__   88__dP 
+Yb  "88  dP__Yb  88YbdP88 88""       Yb   dP   YbdP   88""   88"Yb  
+ YboodP dP""""Yb 88 YY 88 888888      YbodP     YP    888888 88  Yb 
+'''
+
+# messages to print for users according to his choices
+opening_msg = star_wars_ascii_art + "\nA terrible virus variant strikes back somewhere far in the galaxy.." \
+              + "\nYour mission is to reduce the chances of getting infected!"
+chosen_yoda_msg = yoda_ascii_art + "\n" + gameover_ascii_art \
+                  + f"\nGame Over! {p2} has attend the Jedi council and got infected"
+chosen_vader_msg = darth_vader_ascii_art \
+                   + f"\nSmart Choice! {p1} wears a mask, socially and emotionally distant, and follow orders" \
+                   + f"\n2) {p1} has some free time on his spaceship."
+chosen_zoom = zoom_ascii_art \
+              + f"\n{p1} has forgotten to turn of the video camera, so the emperor saw him with his cool pajamas"
+chosen_tiktok = tiktok_ascii_art + "\n" + gameover_ascii_art \
+                + f"\nGame Over! {p1} stepped over his cloak and fell down on the ground." \
+                + "\nTherefore, He has arrived at the hospital for an urgent surgery and got infected.."
+chosen_bat = bat_ascii_art + "\n" + gameover_ascii_art \
+             + f"\nGame Over! {bat} is made from bats.. {p1} got infected from the soup"
+chosen_garlic = garlic_ascii_art + "\n" + win_ascii_art \
+                + f"\nYou Win! {p1} has just been ate {garlic}" \
+                  f", and that keeps everyone else at a safe distance!\nYou are safe from the virus!"
+chosen_apple = apple_ascii_art + "\n" + gameover_ascii_art + \
+               f'\nGame Over! Have you not read "{snow_white}"? the {apple} is poisoned.\nNo one cane wake {p1} ' \
+               f'with a kiss, because of the Recommended preventive measures against the virus!'
+wrong_input = gameover_ascii_art + "\n\nGame Over! Wrong input. Bye."
+
+print(opening_msg)  # program opening message
+
+# ask for an input of a choice
+chosen_character = input(f'1) Choose a Star Wars character carefully! Type "{p1}" or "{p2}"\n')
+if chosen_character.lower() == p1.lower() or chosen_character.lower() == "1":
+    print(chosen_vader_msg)
+    # ask for an input of a choice
+    chosen_activity = input(
+        f'Type "{zoom}" to attend an online meeting with The Emperor. Type "{tiktok}" to learn a new {tiktok} dance\n')
+    if chosen_activity.lower() == zoom.lower() or chosen_activity.lower() == "1":
+        print(chosen_zoom)
+        # ask for an input of a choice
+        chosen_food = input(
+            f'3) {p1} is hungry. He can only choose between 3 options: "{bat}", "{garlic}", '
+            f'and "{apple}"\nWhich one to choose?\n')
+        if chosen_food.lower() == bat.lower() or chosen_food.lower() == "1":
+            print(chosen_bat)
+        elif chosen_food.lower() == garlic.lower() or chosen_food.lower() == "2":
+            print(chosen_garlic)
+        elif chosen_food.lower() == apple.lower() or chosen_food.lower() == "3":
+            print(chosen_apple)
+        else:
+            print(wrong_input)
+    elif chosen_activity.lower() == tiktok.lower() or chosen_activity.lower() == "2":
+        print(chosen_tiktok)
+    else:
+        print(wrong_input)
+elif chosen_character.lower() == p2.lower() or chosen_character.lower() == "2":
+    print(chosen_yoda_msg)
+else:
+    print(wrong_input)
